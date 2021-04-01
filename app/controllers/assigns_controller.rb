7c86fef98cc30fe14ed9d5cb4e0c1edc527d0027
jun kobayashi <junkobayashi1025@gmail.com>
2021-04-01 09:33:12 +0000

    ownerは削除できない

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -16,17 +16,12 @@ class AssignsController < ApplicationController
       end
     end
 
-
-    # user = User.find_or_create_by_email(assign_params)
-    # team.invite_member(user)
-    # redirect_to team_url(team), notice: "ユーザー「#{user.name}」を招待しました"
-  # end
-
   def destroy
     assign = Assign.find(params[:id])
-    assign.destroy
-    redirect_to team_url(params[:team_id])
-    flash[:success] = "ユーザー「#{assign.user.name}」を削除しました"
+    if assign.user == current_user || assign.team.owner == current_user
+      destroy_message = assign_destroy(assign, assign.user)
+      redirect_to team_url(params[:team_id]), notice: destroy_message
+    end
   end
 
   private
@@ -34,11 +29,21 @@ class AssignsController < ApplicationController
     params[:email]
   end
 
+  def assign_destroy(assign, assigned_user)
+   if assigned_user == assign.team.owner
+     flash[:notice] = "責任者は削除できません"
+   elsif assign.destroy
+     flash[:success] = "ユーザー「#{assign.user.name}」を削除しました"
+   else
+     flash[:danger] = "削除に失敗しました"
+   end
+ end
+
   def email_exist?
     team = find_team(params[:team_id])
     if team.assign_users.exists?(email: params[:email])
       redirect_to team_url(team)
-        flash[:danger] = "ユーザーは招待済みです"
+      flash[:danger] = "ユーザーは招待済みです"
     end
   end
 
@@ -50,10 +55,11 @@ class AssignsController < ApplicationController
     team = find_team(params[:team_id])
     unless User.exists?(email: params[:email])
       redirect_to team_url(team)
+      flash[:danger] = "ユーザーは存在しません"
     end
   end
 
   def find_team(team_id)
-   team = Team.find(params[:team_id])
+   　team = Team.find(params[:team_id])
  end
 end
