0d14acd78723ebd99018c86b24b6b767ad40f4e2
jun kobayashi <junkobayashi1025@gmail.com>
2021-04-03 12:48:43 +0000

    提出期限をdueにまとめる

--- app/models/report.rb
+++ app/models/report.rb
@@ -12,21 +12,21 @@ class Report < ApplicationRecord
   scope :sort_by_deadline_date_asc, lambda { all.sort_by(&:deadline_date) }
   scope :sort_by_deadline_date_desc, lambda { all.sort_by(&:deadline_date).reverse }
 
-  def deadline_date
-    if self.checkbox_final
-      return '--------------------'
-    elsif self.checkbox_first
-     if self.checkbox_interim && self.confirmed_date
-       return (self.confirmed_date).strftime("%Y年 %m月 %d日")
-     elsif self.checkbox_interim && self.confirmed_date.nil?
-       return '未定'
-     else
-       return (self.due).strftime("%Y年 %m月 %d日")
-     end
-    else
-     return (self.due).strftime("%Y年 %m月 %d日")
-    end
-  end
+  # def deadline_date
+  #   if self.checkbox_final
+  #     return '--------------------'
+  #   elsif self.checkbox_first
+  #    if self.checkbox_interim && self.confirmed_date
+  #      return (self.confirmed_date).strftime("%Y年 %m月 %d日")
+  #    elsif self.checkbox_interim && self.confirmed_date.nil?
+  #      return '未定'
+  #    else
+  #      return (self.due).strftime("%Y年 %m月 %d日")
+  #    end
+  #   else
+  #    return (self.due).strftime("%Y年 %m月 %d日")
+  #   end
+  # end
 
   def build_attachment_for_form
    self.attachments.build if saved_attachments.length < 5 && unsaved_attachments.length == 0
@@ -46,7 +46,7 @@ class Report < ApplicationRecord
 
   def step_string
     if self.checkbox_interim && self.checkbox_first
-      return self.update(step:"3: 有効性の確認")
+      return self.update(step:"3: 有効性の確認", due: self.confirmed_date)
     elsif self.checkbox_first
       return self.update(step:"2: 中間報告", due: self.accrual_date + 14.day )
     else
