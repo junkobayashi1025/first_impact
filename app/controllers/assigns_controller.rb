8ead993add113f0713d433bdd585e19cbc1621c2
jun kobayashi <junkobayashi1025@gmail.com>
2021-04-06 11:41:20 +0000

    作成中の報告書ある場合、そのユーザーはチームから離脱できない

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -18,9 +18,15 @@ class AssignsController < ApplicationController
 
   def destroy
     assign = Assign.find(params[:id])
-    if assign.user == current_user || assign.team.owner == current_user
-      destroy_message = assign_destroy(assign, assign.user)
-      redirect_to team_url(params[:team_id]), notice: destroy_message
+    undone_reports = assign.user.reports.where(checkbox_final: false)
+    if undone_reports.count == 0
+      if assign.user == current_user || assign.team.owner == current_user
+        destroy_message = assign_destroy(assign, assign.user)
+        redirect_to team_url(params[:team_id])
+      end
+    else
+      redirect_to user_path(assign.user)
+      flash[:danger] = "未完の報告書があるので、ユーザー削除できません"
     end
   end
 
