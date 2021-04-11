d74fbd6e4094157b4c88e4fd899762d07edbd379
jun kobayashi <junkobayashi1025@gmail.com>
2021-04-11 06:16:53 +0000

    ブックマーク機能のバリデーション設定

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,16 +1,6 @@
 class BookmarksController < ApplicationController
-  # before_action :authenticate_user
+  before_action :authenticate_user!
   before_action :report_params, only: [:create, :destroy]
-
-  # def show
-  #   @bookmark = current_user.bookmarks.build(bookmark_params)
-  #     # binding.pry
-  #   @report = @bookmark.post
-  #
-  #   if @bookmark.save
-  #     respond_to :js
-  #   end
-  # end
 
   def create
     if current_user != @report.team.owner && current_user != @report.user
@@ -18,23 +8,15 @@ class BookmarksController < ApplicationController
     else
       redirect_to reports_path, notice:"権限がありません"
     end
-    # if @bookmark.save
-    #   respond_to :js
-    # end
   end
 
   def destroy
-    Bookmark.find_by(user_id: current_user.id, report_id: params[:id]).destroy
-    # if @bookmark.destroy
-    #   respond_to :js
-    # end
+    if current_user != @report.team.owner && current_user != @report.user
+     Bookmark.find_by(user_id: current_user.id, report_id: params[:id]).destroy
+   else
+     redirect_to reports_path, notice:"権限がありません"
+   end
   end
-
-  # def destroy
-  #   @bookmark = Bookmark.find(params[:id])
-  #   @report = @bookmark.post
-
-  # end
 
   private
   def report_params
