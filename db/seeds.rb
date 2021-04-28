4a3a86743fd05bb4d32eeb682003e95ac5b8e02a
jun kobayashi <junkobayashi1025@gmail.com>
2021-04-28 16:49:16 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
-User.find_or_create_by!(email: 'admin@example.com') do |user|
-  user.name = 'admin'
-  user.admin = true
-  user.password = 'admin@example.com'
-  user.password_confirmation = 'admin@example.com'
-end
+# User.find_or_create_by!(email: 'admin@example.com') do |user|
+#   user.name = 'admin'
+#   user.admin = true
+#   user.password = 'admin@example.com'
+#   user.password_confirmation = 'admin@example.com'
+# end
 
 # 20.times do |n|
 #   name = Faker::Name.name
